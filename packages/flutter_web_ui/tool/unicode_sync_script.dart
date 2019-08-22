473500020785fe4ad7aeded6ced8acf5b9438726
yjbanov <yjbanov@google.com>
2019-08-22 09:20:09 -0700

    Fix copyright headers and add a license_test.dart
    PiperOrigin-RevId: 264850502

--- packages/flutter_web_ui/tool/unicode_sync_script.dart
+++ packages/flutter_web_ui/tool/unicode_sync_script.dart
@@ -1,4 +1,4 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
+// Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -103,7 +103,7 @@ class WordBreakPropertiesSyncer extends PropertiesSyncer {
   @override
   String template(List<String> header, List<PropertyTuple> data) {
     return '''
-// Copyright 2019 The Chromium Authors. All rights reserved.
+// Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
